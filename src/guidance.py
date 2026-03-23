4ee9c4193ba42874d91e8993c2dc89cfe5cb4842
Thibault Schiesser <thibault.schiesser@epfl.ch>
2026-03-23 11:05:12 +0100

    add linear increasing + exponential increasing guidance

--- src/guidance.py
+++ src/guidance.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def constant_guidance(noise_pred_uncond, noise_pred_text, guidance_scale):
     """
     Applies constant guidance to the noise prediction.
@@ -10,4 +12,47 @@ def constant_guidance(noise_pred_uncond, noise_pred_text, guidance_scale):
     Returns:
         The guided noise prediction.
     """
-    return noise_pred_uncond + (noise_pred_text - noise_pred_uncond) * guidance_scale
+    omega = guidance_scale
+    return noise_pred_uncond + (noise_pred_text - noise_pred_uncond) * omega
+
+def linear_guidance(noise_pred_uncond, noise_pred_text, guidance_scale, current_step, total_steps):
+    """ 
+    Applies increasing linear guidance to the noise prediction.
+    It satisfies: 
+        integral from 0 to T of omega(t) = guidance_scale * T.
+    
+    Args:
+        noise_pred_uncond: The noise prediction for the unconditional input.
+        noise_pred_text: The noise prediction for the text input.
+        guidance_scale: The maximum scale of the guidance to apply.
+        current_step: The current inference step.
+        total_steps: The total number of inference steps.
+    
+    Returns:
+        The guided noise prediction.
+    """
+    # Calculate the linear scaling factor based on the current step
+    omega = 2 * (1 - current_step / total_steps) * guidance_scale
+    return noise_pred_uncond + (noise_pred_text - noise_pred_uncond) * omega
+
+def exponential_guidance(noise_pred_uncond, noise_pred_text, guidance_scale, time, t_max):
+    """ 
+    Applies increasing exponential guidance to the noise prediction.
+    It satisfies: 
+        integral from 0 to T of omega(t) = guidance_scale * T.
+    
+    Args:
+        noise_pred_uncond: The noise prediction for the unconditional input.
+        noise_pred_text: The noise prediction for the text input.
+        guidance_scale: The maximum scale of the guidance to apply.
+        time: The current time step.
+        t_max: The maximum time step. 
+            time < t_max !
+    
+    Returns:
+        The guided noise prediction.
+    """
+    # Calculate the exponential scaling factor based on the current step
+    alpha = (guidance_scale*t_max + 1 - np.exp(t_max)) / (- t_max)
+    omega = alpha * (np.exp(time))
+    return noise_pred_uncond + (noise_pred_text - noise_pred_uncond) * omega
