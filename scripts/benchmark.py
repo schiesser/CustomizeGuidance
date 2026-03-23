c532c4f53184b04c922ea30d4be4a8dd6d57a0c6
Thibault Schiesser <thibault.schiesser@epfl.ch>
2026-03-23 08:48:52 +0100

    add script for benchmarking

---
+++ scripts/benchmark.py
@@ -0,0 +1,42 @@
+import argparse
+from src.run import benchmark
+
+benchmark_parser = argparse.ArgumentParser(description='T2I Benchmark.')
+
+benchmark_parser.add_argument("--model", type=str, default="SD3")
+benchmark_parser.add_argument("--guidance_types", type=str, nargs="+", required=True)
+benchmark_parser.add_argument("--model_path", type=str, required=True)
+benchmark_parser.add_argument("--data_annotations_path", type=str, required=True)
+benchmark_parser.add_argument("--data_images_path", type=str, required=True)
+benchmark_parser.add_argument("--height", type=int, default=512)
+benchmark_parser.add_argument("--width", type=int, default=512)
+benchmark_parser.add_argument("--num_inference_steps", type=int, default=28)
+benchmark_parser.add_argument("--guidance_scale", type=float, default=7.0)
+benchmark_parser.add_argument("--score_list", type=str, nargs="+", default=["FID"])
+benchmark_parser.add_argument("--number_of_images", type=int, default=5000)
+benchmark_parser.add_argument("--run_id", type=str, default="test_run")
+benchmark_parser.add_argument("--clip_model_path", type=str, default=None)
+benchmark_parser.add_argument("--blip_model_path", type=str, default=None)
+benchmark_parser.add_argument("--seed", type=int, default=13)
+
+args = benchmark_parser.parse_args()
+
+scores = benchmark(
+    model=args.model,
+    guidance_types=args.guidance_types,
+    model_path=args.model_path,
+    data_annotations_path=args.data_annotations_path,
+    data_images_path=args.data_images_path,
+    height=args.height,
+    width=args.width,
+    num_inference_steps=args.num_inference_steps,
+    guidance_scale=args.guidance_scale,
+    score_list=args.score_list,
+    number_of_images=args.number_of_images,
+    run_id=args.run_id,
+    clip_model_path=args.clip_model_path,
+    blip_model_path=args.blip_model_path,
+    seed=args.seed,
+)
+
+print(scores)
